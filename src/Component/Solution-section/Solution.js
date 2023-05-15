e5e2b61c001b07f6d263c56488ba1fa676e7fda3
Aya Shahal <ayashahal055@gmail.com>
2023-05-15 15:42:50 +0300

    solution section

---
+++ src/Component/Solution-section/Solution.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Solution.css';
+function Solution() {
+    return (
+       
+        <div className='solution-section'>
+            <div className='solution-title'>
+                <h1> The Solution</h1>
+                <p>Share more,</p>
+               <p>waste less</p>
+            </div>
+            <div className='paragraph-section'>
+            <p>Let’s normalize sharing what we no longer need and make buying new a last resort</p>
+            </div>
+          
+        </div>
+
+    );
+}
+
+export default Solution;
