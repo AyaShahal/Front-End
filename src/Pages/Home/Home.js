7152cc2f3af552f382620afa4e4d940eafa94b9c
Aya Shahal <ayashahal055@gmail.com>
2023-05-15 15:43:18 +0300

    home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Hero from '../../Component/Home-hero-section/hero';
+import Ambition from '../../Component/Ambition-section/Ambition';
+import Problem from '../../Component/Problem-section/Problem';
+import Solution from '../../Component/Solution-section/Solution';
 function Home() {
     return (
         <div>
         <Hero/>
+        <Ambition/>
+        <Problem/>
+        <Solution/>
         </div>
     );
 }
