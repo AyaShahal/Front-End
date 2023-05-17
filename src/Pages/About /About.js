a58a395352db3b55ebdb242fb2de2ee9d19cda17
Aya Shahal <ayashahal055@gmail.com>
2023-05-17 09:54:09 +0300

    about page section

--- src/Pages/About /About.js
+++ src/Pages/About /About.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Abouthero from '../../Component/About-Hero-section/About-hero';
+import Aboutsection from '../../Component/About-section/About';
+import Join from '../../Component/Join us/join-us';
 function About() {
     return (
         <div>
-            About
+            <Abouthero/>
+            <Aboutsection/>
+            <Join/>
         </div>
     );
 }
