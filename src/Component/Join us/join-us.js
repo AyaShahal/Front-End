98a901928f65fab41e4b212c6b5ee94da65884aa
Aya Shahal <ayashahal055@gmail.com>
2023-05-17 09:53:59 +0300

    join us section

---
+++ src/Component/Join us/join-us.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './join-us.css';
+function Join() {
+  return (
+    <div className="join">
+      <h1>Join Us</h1>
+      <span>Join For Better Future</span>
+      <p>
+        We invite you to join us on this journey towards a more sustainable and
+        equitable world. Together, we can create lasting change, one surplus
+        meal at a time. Whether you are a business with surplus food or a
+        charitable organization committed to alleviating hunger, our platform
+        provides the tools and resources to make a meaningful impact
+      </p>
+      <button className="btn btn-primary">Join Now</button>
+    </div>
+  );
+}
+
+export default Join;
