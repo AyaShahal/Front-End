7d9630cc9d3a284d059d16aa4a1ec13be637f0a9
Aya Shahal <ayashahal055@gmail.com>
2023-05-17 09:53:46 +0300

    home hero section

--- src/Component/Home-hero-section/hero.js
+++ src/Component/Home-hero-section/hero.js
@@ -3,8 +3,8 @@ import './hero.css'
 function hero() {
     return (
        <div className='hero'>
-        <div class="header-container">
-        <div class="header-container-inner">
+        <div className="header-container">
+        <div className="header-container-inner">
           <h1>JOIN US TO FIGHT FOOD WASTE TOGETHER</h1>
           <p>
           Join our movement to reduce food waste and create a sustainable future.
@@ -12,7 +12,7 @@ function hero() {
           going to waste. Help us build a world where everyone has access to
           nutritious meals while protecting our planet.
           </p>
-          <a href="#" class="btn">JOIN US</a>
+          <a href="#" className="btn">JOIN US</a>
         </div>
       </div>
       </div>
