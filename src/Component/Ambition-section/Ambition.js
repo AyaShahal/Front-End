59c6780b6a5ac182ed4ff146c6a9300781fea9f6
Aya Shahal <ayashahal055@gmail.com>
2023-05-18 10:09:04 +0300

    ambition section

--- src/Component/Ambition-section/Ambition.js
+++ src/Component/Ambition-section/Ambition.js
@@ -21,9 +21,6 @@ function Ambition() {
         <h1>Mission</h1>
         <p>Bridging surplus food and charitable organizations to reduce waste, protect the environment, and promote social responsibility</p>
       </div>
-      <div class="face back">
-        <p>Our mission is to bridge the gap between surplus food and charitable organizations, aiming to achieve multiple positive outcomes. Firstly, we strive to reduce food waste by connecting businesses, restaurants, and other food providers that have excess food with charitable organizations that can efficiently distribute it to those in need.</p>
-      </div>
     </div>
   </div>
   <div class="card-space">
@@ -33,9 +30,6 @@ function Ambition() {
         <h1>Vision</h1>
         <p>Revolutionizing food waste reduction through technology, collaboration, and social responsibility.</p>
       </div>
-      <div class="face back">
-        <p>Our organization is dedicated to revolutionizing food waste reduction by leveraging technology, fostering collaboration, and promoting social responsibility. We believe that by combining these elements, we can create a substantial and sustainable impact on reducing food waste and its associated negative consequences.</p>
-      </div>
     </div>
   </div>
   <div class="card-space">
@@ -44,9 +38,6 @@ function Ambition() {
       <img class="tgtg-icon tgtg-stats-promo__icon" src={icon2} alt="" width="136" height="136"/>
         <h1>Impact</h1>
         <p>Our platform aims to have a significant impact by reducing food waste, minimizing environmental degradation, and fostering social responsibility.</p>
-      </div>
-      <div class="face back">
-        <p>At our platform, we have a clear objective: to make a significant impact by reducing food waste, minimizing environmental degradation, and fostering social responsibility. We recognize the interconnectedness of these goals and strive to address them in a comprehensive and integrated manner.</p>
       </div>
     </div>
   </div>
