f24c37e2b9ebca35f802f78e033d7a8e900d934b
Aya Shahal <ayashahal055@gmail.com>
2023-05-19 21:09:44 +0300

    user profile page

---
+++ src/Pages/User-Profile/User-Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import User from '../../Component/User-profile/user-profile'
+
+function Dashboard() {
+    return (
+        <div>
+          <User/>
+        </div>
+    );
+}
+
+export default Dashboard;
