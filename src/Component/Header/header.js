933196f7c2153c94f78eb79ed0aeb3914e296796
Aya Shahal <ayashahal055@gmail.com>
2023-05-19 21:09:22 +0300

    header

--- src/Component/Header/header.js
+++ src/Component/Header/header.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import "./header.css";
-import logo from '../Savior-removebg-preview.png'
-function header() {
+import logo from "../Savior-removebg-preview.png";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+function Header() {
+  const [isSelectorOpen, setIsSelectorOpen] = useState(false);
+
+  function toggleSelector() {
+    setIsSelectorOpen(!isSelectorOpen);
+  }
+  const closeToggle = () => {
+    setIsSelectorOpen(false);
+  };
   return (
     <header>
       <div className="navbar">
         <nav>
           <div className="nav-link">
-          <img
-            src={logo}
-            alt=""
-            height="100px"
-            width="100px"
-          />
-            <NavLink to="/ " className="links">
+            <img src={logo} alt="" height="100px" width="100px" />
+            <NavLink to="/" className="links">
               Home
             </NavLink>
             <NavLink to="/About" className="links">
@@ -24,7 +30,7 @@ function header() {
               Surplus Food
             </NavLink>
             <NavLink to="/Contact Us" className="links">
-            Contact Us
+              Contact Us
             </NavLink>
           </div>
         </nav>
@@ -35,10 +41,28 @@ function header() {
           <NavLink to="/Signup" className="button">
             Sign Up
           </NavLink>
+          <div className="user-profile">
+      <div className="user">
+        <FontAwesomeIcon icon={faUser} />
+        <div className="profile-selector" onClick={toggleSelector}>
+          <FontAwesomeIcon icon={faCaretDown} />
+        </div>
+      </div>
+      {isSelectorOpen && (
+        <div className="selector-content">
+          <NavLink to="/userProfile" className="selector-link" onClick={closeToggle}>
+            View Profile
+          </NavLink>
+          <NavLink to="/Logout" className="selector-link" onClick={closeToggle}>
+            Logout
+          </NavLink>
+        </div>
+      )}
+    </div>
         </div>
       </div>
     </header>
   );
 }
 
-export default header;
+export default Header;
