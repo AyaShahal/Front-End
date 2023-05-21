a3a6282244cc37e77d3c14b43d0979951b973f80
Aya Shahal <ayashahal055@gmail.com>
2023-05-21 14:53:36 +0300

    dashboard

---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Dashboard.css';
+import { Outlet } from "react-router-dom";
+import Sidebar from '../../Component/Sidebar/Sidebar';
+
+
+function Dashboard() {
+    return (
+        <div className="layout">
+      <Sidebar />
+      <div className="content">
+        <Outlet /> 
+      </div>
+    </div>
+    );
+}
+
+export default Dashboard;
