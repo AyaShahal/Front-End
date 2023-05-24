54ddc3c73ca08acbd410e2a34d1d2b2e1a9934e7
Aya Shahal <ayashahal055@gmail.com>
2023-05-24 16:18:24 +0300

    login done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Signin from '../../Component/Signin/Signin';
-
+import { UserProvider } from "../../Component/UserProvider";
 function Login() {
     return (
         <div>
+              <UserProvider>
         <Signin/>
+        </UserProvider>
         </div>
     );
 }
