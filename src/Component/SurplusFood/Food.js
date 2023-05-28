23dd4e7babbc81b76c93968ffa515e82185372ff
Aya Shahal <ayashahal055@gmail.com>
2023-05-28 19:06:54 +0300

    product page

--- src/Component/SurplusFood/Food.js
+++ src/Component/SurplusFood/Food.js
@@ -1,21 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "./Food.css";
+import axios from "axios";
 
 function Surplus() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCardId, setSelectedCardId] = useState(null);
+  const [selectedCardDetails, setSelectedCardDetails] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState("");
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+  const [data, setData] = useState([]);
+
+ 
 
   const closeModal = () => {
+    setSelectedCardId(null);
+    setSelectedCardDetails(null);
     setIsModalOpen(false);
   };
+
   const handleCategoryChange = (event) => {
     const selectedValue = event.target.value;
     setSelectedCategory(selectedValue);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:7000/api/Food");
+        console.log(response);
+        setData(response.data);
+      } catch (error) {
+        console.log("An error occurred while fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const openModal = (cardId) => {
+    console.log("Selected Card ID:", cardId);
+  
+    const selectedCard = data.find((item) => item._id === cardId);
+    console.log("Selected Card:", selectedCard);
+  
+    if (selectedCard) {
+      setSelectedCardId(cardId);
+      setSelectedCardDetails(selectedCard);
+      setIsModalOpen(true);
+    } else {
+      console.log('Item not found!');
+    }
+  };
+  
   return (
     <div>
       <div className="wrapper">
@@ -30,177 +65,106 @@ function Surplus() {
           </nav>
         </div>
         <div className="Categories-dropdown">
-            <select
-              id="categorySelect"
-              value={selectedCategory}
-              onChange={handleCategoryChange}
-            >
-              <option value="">All Products</option>
-              <option value="Homemade">Homemade</option>
-              <option value="Recycling">Recycling</option>
-              <option value="Hygienic">Hygienic</option>
-            </select>
-          </div>
+          <select
+            id="categorySelect"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+          >
+            <option value="">All Products</option>
+            <option value="Homemade">Homemade</option>
+            <option value="Recycling">Recycling</option>
+            <option value="Hygienic">Hygienic</option>
+          </select>
+        </div>
 
         <div className="cards">
-          <div className="card1">
-            <div className="card__image">
-              <img
-                src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="Salad"
-              />
-            </div>
-            <div className="card__info">
-              <div className="card__info--title">
-                <h3>Salad</h3>
-                <div className="Food__info">
-                  <p>Fresh & sweet</p>
-                  <button className="btn-primary1" onClick={openModal}>
-                    View
-                  </button>
+          {data.map((item) => (
+            <div className="card1" key={item.id}>
+              <div className="card__image">
+                <img
+                  src={"http://localhost:7000/" + item.image}
+                  alt={item.title}
+                  width="370px"
+                />
+              </div>
+              <div className="card__info">
+                <div className="card__info--title">
+                  <h3>{item.name}</h3>
+                  <div className="Food__info">
+                    <p>{item.description}</p>
+                    <button
+                      className="btn-primary1"
+                      onClick={() => openModal(item._id)}
+                    >
+                      View
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="card1">
-            <div className="card__image">
-              <img
-                src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="Salad"
-              />
-            </div>
-            <div className="card__info">
-              <div className="card__info--title">
-                <h3>Salad</h3>
-                <div className="Food__info">
-                  <p>Fresh & sweet</p>
-                  <button className="btn-primary1" onClick={openModal}>
-                    View
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card1">
-            <div className="card__image">
-              <img
-                src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="Salad"
-              />
-            </div>
-            <div className="card__info">
-              <div className="card__info--title">
-                <h3>Salad</h3>
-                <div className="Food__info">
-                  <p>Fresh & sweet</p>
-                  <button className="btn-primary1" onClick={openModal}>
-                    View
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card1">
-            <div className="card__image">
-              <img
-                src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="Salad"
-              />
-            </div>
-            <div className="card__info">
-              <div className="card__info--title">
-                <h3>Salad</h3>
-                <div className="Food__info">
-                  <p>Fresh & sweet</p>
-                  <button className="btn-primary1" onClick={openModal}>
-                    View
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card1">
-            <div className="card__image">
-              <img
-                src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="Salad"
-              />
-            </div>
-            <div className="card__info">
-              <div className="card__info--title">
-                <h3>Salad</h3>
-                <div className="Food__info">
-                  <p>Fresh & sweet</p>
-                  <button className="btn-primary1" onClick={openModal}>
-                    View
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
-        {isModalOpen && (
+
+        {isModalOpen && selectedCardDetails && (
           <div className="modal">
             <div className="modal-content">
-            <span className="close1" onClick={closeModal}>
+              <span className="close1" onClick={closeModal}>
                 &times;
               </span>
               <div className="card2__image">
                 <img
-                  src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  alt="Salad" width="500px" height="333px"
+                  src={"http://localhost:7000/" + selectedCardDetails.image}
+                  alt={selectedCardDetails.name}
+                  width="500px"
+                  height="333px"
                 />
                 <div className="card__info--title">
                   <div className="info">
                     <div className="product-name">
-                      <h3>Salad</h3>
+                      <h3>{selectedCardDetails.name}</h3>
                     </div>
                     <div className="details1">
                       <span>Quantity: </span>
-                      <p> 10</p>
+                      <p>{selectedCardDetails.quantity}</p>
                     </div>
                     <div className="details1">
                       <span>Expiration Date: </span>
-                      <p> 2023-05-31</p>
+                      <p>{selectedCardDetails.expirydate.split("T")[0]}</p>
                     </div>
                   </div>
                   <div className="Food__info1">
-                    <p>Fresh &amp; sweet</p>
-                    <p>
-                      Food Description: Lorem ipsum dolor sit amet, consectetur
-                      adipiscing elit.
-                    </p>
-                    
+                    <p>{selectedCardDetails.description}</p>
+                    <p>Food Description: {selectedCardDetails.description}</p>
                   </div>
                 </div>
               </div>
               <div className="more-info">
-                <h2>Business Name</h2>
+                <h2>{selectedCardDetails.User.username}</h2>
                 <div className="details">
-                  <span>City: </span> <p>New York</p>
+                  <span>City: </span>{" "}
+                  <p>{selectedCardDetails.User.address[0].city}</p>
                 </div>
                 <div className="details">
-                  <span>Building: </span> <p> ABC Building</p>
+                  <span>Building: </span>{" "}
+                  <p>{selectedCardDetails.User.address[0].building}</p>
                 </div>
 
                 <div className="details">
-                  <span>Street: </span> <p> Main Street</p>
+                  <span>Street: </span> <p>Main Street</p>
                 </div>
                 <div className="details">
-                  <span>Phone Number: </span> <p>123-456-7890</p>
+                  <span>Phone Number: </span>{" "}
+                  <p>{selectedCardDetails.User.phone}</p>
                 </div>
                 <div className="donate-button">
-                <button className="btn-primary1" onClick={openModal}>
-                  Donate
-                  </button>
-                  </div>
+                  <button className="btn-primary1">Donate</button>
+                </div>
               </div>
+
               <span className="close" onClick={closeModal}>
                 &times;
               </span>
-
             </div>
-            
           </div>
         )}
       </div>
