fbeb91e7e39e98758ef1ce345728ae05a2ce0140
Aya Shahal <ayashahal055@gmail.com>
2023-05-28 19:02:38 +0300

    404 page

--- src/Pages/404 /404.js
+++ src/Pages/404 /404.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './404.css';
 
 function NotFound() {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="not-found">
+      <h1 className="not-found-title">404</h1>
+      <p className="not-found-text">Page not found.</p>
+      <Link to="/" className="not-found-link">Go back to Home page</Link>
+    </div>
+  );
 }
 
 export default NotFound;
