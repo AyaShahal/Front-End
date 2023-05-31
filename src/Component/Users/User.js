f9f61e3ec14e3b525c9f3e00bb1cd403775f1977
Aya Shahal <ayashahal055@gmail.com>
2023-05-31 11:57:48 +0300

    user dashboard page

--- src/Component/Users/User.js
+++ src/Component/Users/User.js
@@ -1,11 +1,91 @@
-import React from 'react';
+
+import React, { useState, useEffect, useRef } from "react";
+import MaterialReactTable from "material-react-table";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Cookies from 'js-cookie';
+import Loader from "../Loader/loader";
+import "./User.css";
 
 function User(props) {
-    return (
-        <div>
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [formattedColumns, setColumns] = useState([]);
+ 
+  const token = Cookies.get('jwt');
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios
+      .get("https://surplus-app-api.onrender.com/api/user", )
+      .then((response) => {
+        console.log(response.data);
+        if (response.data && Array.isArray(response.data.response)) {
+          const formattedColumns = [
+           
+            { accessorKey: "username", header: "userName" },
+            { accessorKey: "email", header: "Email" },
+            { accessorKey: "role", header: "Role" },
             
+            {
+              accessorKey: "createdAt",
+              header: "created AT",
+              enableEditing: false,
+            },
+            {
+              accessorKey: "updatedAt",
+              header: "updated AT",
+              enableEditing: false,
+            },
+          ];
+
+          setColumns(formattedColumns);
+          setData(response.data.response);
+        } else {
+          console.error("Invalid response format");
+          setData([]);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+ 
+
+ 
+
+  
+  return (
+    loading ? (
+      <Loader />
+    ) : (
+      <div className="dash-main">
+        <div className="category">
+   
+         
         </div>
-    );
+        <MaterialReactTable
+          columns={formattedColumns}
+          data={data}
+          enableColumnOrdering
+          enablePagination={true}
+        
+          
+        />
+
+      </div>
+    )
+  );
+ 
+  
 }
+
+
 
 export default User;
