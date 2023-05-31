7eebd090fa49e8cb6f0bd67a3bd136e41320019b
Aya Shahal <ayashahal055@gmail.com>
2023-05-31 11:56:49 +0300

    home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,6 @@ import Hero from '../../Component/Home-hero-section/hero';
 import Ambition from '../../Component/Ambition-section/Ambition';
 import Problem from '../../Component/Problem-section/Problem';
 import Solution from '../../Component/Solution-section/Solution';
-import ContactUs from '../../Component/ContactUs-section/ContactUs';
 function Home() {
     return (
         <div>
